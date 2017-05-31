a77bbb5d5c1e089e26058669bb8997eb65eeb0e8
(cdep)illabout <cdep.illabout@gmail.com>
2017-05-31 19:53:51 +0900

    Try something else.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,27 +1,49 @@
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE RankNTypes #-}
 
 module Lib where
 
 import Control.Lens (Field1, Field2, Lens, Lens', ALens', _1, _2, cloneLens)
+import Control.Lens.Internal.Context
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
-incrementThrough :: (Int, Int) -> Lens' (Int, Int) Int -> IO (Int, Int)
-incrementThrough a l = l (\b -> b + 1 <$ print b) a
-
-incrementThrough' :: (Int, Int) -> ALens' (Int, Int) Int -> IO (Int, Int)
-incrementThrough' a l = cloneLens l (\b -> b + 1 <$ print b) a
-
 tuple :: (Int, Int)
 tuple = (1, 2)
 
+---------------------------------------------------------------------
+
+incrementThroughExpandLens :: (forall f. Functor f => (Int -> f Int) -> (Int, Int) -> f (Int, Int)) -> IO (Int, Int)
+incrementThroughExpandLens l = l (\b -> b + 1 <$ print b) tuple
+
+incrementThroughExpandLens' :: ALens' (Int, Int) Int -> IO (Int, Int)
+incrementThroughExpandLens' l = cloneLens l (\b -> b + 1 <$ print b) tuple
+
 testIncrementThrough :: Bool -> IO (Int, Int)
-testIncrementThrough = incrementThrough tuple . bFunc
+testIncrementThrough = incrementThroughExpandLens `comp` bFuncExpandLens
 
 testIncrementThrough' :: Bool -> IO (Int, Int)
-testIncrementThrough' = incrementThrough' tuple . bFunc
+testIncrementThrough' = incrementThroughExpandLens' `comp'` bFuncExpandLens
 
-bFunc :: (Field1 s t a b, Field2 s t a b, s ~ t, a ~ b) => Bool -> Lens' s a
-bFunc b = if b then _1 else _2
+bFuncExpandLens
+  :: forall f.
+     Functor f
+  => Bool -> (Int -> f Int) -> (Int, Int) -> f (Int, Int)
+bFuncExpandLens b = if b then _1 else _2
+
+comp
+  :: ((forall g. Functor g => (Int -> g Int) -> (Int, Int) -> g (Int, Int)) -> IO (Int, Int))
+  -> (forall f. Functor f => Bool -> (Int -> f Int) -> (Int, Int) -> f (Int, Int))
+  -> Bool
+  -> IO (Int, Int)
+comp = (.)
+
+comp'
+  :: (ALens' (Int, Int) Int -> IO (Int, Int))
+  -- -> (Bool -> (Int -> (Pretext (->) Int Int) Int) -> (Int, Int) -> (Pretext (->) Int Int) (Int, Int))
+  -> (forall f. Functor f => Bool -> (Int -> f Int) -> (Int, Int) -> f (Int, Int))
+  -> Bool
+  -> IO (Int, Int)
+comp' = (.)
